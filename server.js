25ff959bd185502ae23ff6018115f659ad2dfae7
unknown <kleber@gmail.com>
2024-04-30 02:01:58 -0300

    Nome

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/listUsers', function (req, res) {
 
 app.get('/listUser/:id', function (req, res) {
     // First read existing users.
-    //ultima mudanca
+    //ultima mudancaHJ
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        var users = JSON.parse( data );
        var user = users["user" + req.params.id] 
